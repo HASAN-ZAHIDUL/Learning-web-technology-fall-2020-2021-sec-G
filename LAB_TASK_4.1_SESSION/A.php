51f99d787108aec07e2b0201000e23f88555f236
unknown <mixmartialartsjoy@gmail.com>
2020-11-03 10:45:46 +0600

    now

---
+++ LAB_TASK_4.1_SESSION/A.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+	<title>PUBLIC HOME</title>
+</head>
+<body>
+	<table border="1" cellspaceing="0px" align="center" width="900px">
+		<tr height="50px">
+			
+		    
+		    <th colspan="3">
+		    	<img src="1.JPG">
+		    	<a href="">Home</a>|
+		    	<a href="">Login</a>|
+		    	<a href="">Registration</a>
+		    </th>
+		    
+   
+		   
+		</tr>
+
+		<tr height="100px">
+			<td colspan="3" style="font-size: 23">
+				Welcome to xCompany
+			</td>
+				
+		</tr>
+
+
+		
+
+		<tr align="center">
+			<td colspan="3">Copyright © 2017</td>
+		</tr>
+
+
+	</table>>
+</body>
+</html>
+
+
